798de0e3b0ef6f1a9dd088272a3b635ad21a553c
jefferykarbowski <58263127+jefferykarbowski@users.noreply.github.com>
2021-05-25 10:40:35 -0400

    Added Styling Defaults in settings

--- carrier-matrix-easy-installation.php
+++ carrier-matrix-easy-installation.php
@@ -10,7 +10,7 @@ Author: Apis Productions
 
 Author URI: http://www.apisproductions.com/
 
-Version: 1.0.11
+Version: 1.1.0
 
 */
 
@@ -26,9 +26,17 @@ class CarrierMatrixEasyInstallation {
             add_action( 'admin_menu', array( $this, 'carrier_matrix_add_plugin_page' ) );
             add_action( 'admin_init', array( $this, 'carrier_matrix_page_init' ) );
             add_action( 'init', array( $this, 'carrier_matrix_gutenberg_block_admin') );
+            add_action( 'admin_enqueue_scripts', array( $this, 'cm_enqueue_color_picker' ) );
         }
         add_shortcode('carrier-matrix', array( $this, 'carrier_matrix_shortcode_handler' ));
     }
+
+
+    public function cm_enqueue_color_picker($hook_suffix) {
+        wp_enqueue_style( 'wp-color-picker' );
+        wp_enqueue_script( 'cm-color-picker', plugins_url('/js/cm-color-picker.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+    }
+
 
     public function carrier_matrix_gutenberg_block_admin() {
 
@@ -79,7 +87,7 @@ class CarrierMatrixEasyInstallation {
     }
 
     public function carrier_matrix_create_admin_page() {
-        $this->carrier_matrix_options = get_option( 'carrier_matrix_option_name' ); ?>
+        $this->carrier_matrix_options = get_option( 'carrier_matrix_options' ); ?>
 
         <div class="wrap">
             <h2>Carrier Matrix Easy Installation</h2>
@@ -99,14 +107,14 @@ class CarrierMatrixEasyInstallation {
     public function carrier_matrix_page_init() {
         register_setting(
             'carrier_matrix_option_group',
-            'carrier_matrix_option_name',
+            'carrier_matrix_options',
             array( $this, 'carrier_matrix_sanitize' )
         );
 
         add_settings_section(
-            'carrier_matrix_setting_section',
+            'carrier_matrix_settings_section',
             'Settings',
-            array( $this, 'carrier_matrix_section_info' ),
+            array( $this, 'carrier_matrix_section_settings' ),
             'carrier-matrix-easy-installation-admin'
         );
 
@@ -115,7 +123,7 @@ class CarrierMatrixEasyInstallation {
             'API Key',
             array( $this, 'cm_api_key_callback' ),
             'carrier-matrix-easy-installation-admin',
-            'carrier_matrix_setting_section'
+            'carrier_matrix_settings_section'
         );
 
         add_settings_field(
@@ -123,62 +131,171 @@ class CarrierMatrixEasyInstallation {
             'Agency ID',
             array( $this, 'cm_agency_id_callback' ),
             'carrier-matrix-easy-installation-admin',
-            'carrier_matrix_setting_section'
+            'carrier_matrix_settings_section'
+        );
+
+        add_settings_section(
+            'carrier_matrix_default_styling_section',
+            'Default Styling',
+            array( $this, 'carrier_matrix_section_default_styling' ),
+            'carrier-matrix-easy-installation-admin'
+        );
+
+
+        add_settings_field(
+            'cm_button_text',
+            'Button Text<br><small>[button-text]</small>',
+            array( $this, 'cm_button_text_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
+        );
+
+        add_settings_field(
+            'cm_results_height',
+            'Min height of the results table<br><small>[button-text]</small>',
+            array( $this, 'cm_results_height_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
+        );
+
+        add_settings_field(
+            'cm_heading_color',
+            'Headings Background Color<br><small>[heading-color]</small>',
+            array( $this, 'cm_heading_color_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
+        );
+
+        add_settings_field(
+            'cm_heading_open_color',
+            'Heading Open Background Color<br><small>[heading-open-color]</small>',
+            array( $this, 'cm_heading_open_color_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
+        );
+
+        add_settings_field(
+            'cm_heading_hover_color',
+            'Heading Hover Background Color<br><small>[heading-hover-color]</small>',
+            array( $this, 'cm_heading_hover_color_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
+        );
+
+        add_settings_field(
+            'cm_heading_text_color',
+            'Heading Text Color<br><small>[heading-text-color]</small>',
+            array( $this, 'cm_heading_text_color_callback' ),
+            'carrier-matrix-easy-installation-admin',
+            'carrier_matrix_default_styling_section'
         );
     }
 
     public function carrier_matrix_sanitize($input) {
         $sanitary_values = array();
-        if ( isset( $input['cm_api_key'] ) ) {
-            $sanitary_values['cm_api_key'] = sanitize_text_field( $input['cm_api_key'] );
-        }
 
-        if ( isset( $input['cm_agency_id'] ) ) {
-            $sanitary_values['cm_agency_id'] = sanitize_text_field( $input['cm_agency_id'] );
-        }
+        $values = array('cm_api_key', 'cm_agency_id', 'cm_button_text', 'cm_results_height', 'cm_heading_color', 'cm_heading_open_color', 'cm_heading_hover_color', 'cm_heading_text_color');
 
+        foreach ($values as $value) {
+            if ( isset( $input[$value] ) ) {
+                $sanitary_values[$value] = sanitize_text_field( $input[$value] );
+            }
+        }
+        
         return $sanitary_values;
     }
 
-    public function carrier_matrix_section_info() {
+    public function carrier_matrix_section_settings() {
         echo '<p>Place the Carrier Matrix shortcode on the pages that you want to give users access to the Carrier Matrix search application. <br>Use either the "form" attribute or the "results" attribute to specify if the shortcode will show the search form or results, and then accompanied by what type of form or results ("product", "abr", "report", or "contact"). <br>e.g.: <strong><br>[carrier-matrix form="product" button_text="Search"]<br>[carrier-matrix results="product"]</strong></p>';
     }
 
+
+    public function carrier_matrix_section_default_styling() {
+        echo '<p>Add site-wide defaults for the styling of the Carrier Matrix, each option can be overwritten within the shortcode as attributes (specified in brackets) if you would like to change the single instance.</p>';
+    }
+
+
     public function cm_api_key_callback() {
         printf(
-            '<input class="regular-text" type="text" name="carrier_matrix_option_name[cm_api_key]" id="cm_api_key" value="%s">',
+            '<input class="regular-text" type="text" name="carrier_matrix_options[cm_api_key]" id="cm_api_key" value="%s">',
             isset( $this->carrier_matrix_options['cm_api_key'] ) ? esc_attr( $this->carrier_matrix_options['cm_api_key']) : ''
         );
     }
 
     public function cm_agency_id_callback() {
         printf(
-            '<input class="regular-text" type="text" name="carrier_matrix_option_name[cm_agency_id]" id="cm_agency_id" value="%s">',
+            '<input class="regular-text" type="text" name="carrier_matrix_options[cm_agency_id]" id="cm_agency_id" value="%s">',
             isset( $this->carrier_matrix_options['cm_agency_id'] ) ? esc_attr( $this->carrier_matrix_options['cm_agency_id']) : ''
         );
     }
 
-    public function carrier_matrix_shortcode_handler($atts) {
+    public function cm_button_text_callback() {
+        printf(
+            '<input class="regular-text" type="text" name="carrier_matrix_options[cm_button_text]" id="cm_button_text" value="%s">',
+            isset( $this->carrier_matrix_options['cm_button_text'] ) ? esc_attr( $this->carrier_matrix_options['cm_button_text']) : 'Search'
+        );
+    }
 
+    public function cm_results_height_callback() {
+        printf(
+            '<input class="regular-text" type="text" name="carrier_matrix_options[cm_results_height]" id="cm_results_height" value="%s">',
+            isset( $this->carrier_matrix_options['cm_results_height'] ) ? esc_attr( $this->carrier_matrix_options['cm_results_height']) : '96vh'
+        );
+    }
+
+    public function cm_heading_color_callback() {
+        printf(
+            '<input class="cm-color-picker" type="text" name="carrier_matrix_options[%s]" id="%s" value="%s">',
+            'cm_heading_color', 'cm_heading_color', isset( $this->carrier_matrix_options['cm_heading_color'] ) ? esc_attr( $this->carrier_matrix_options['cm_heading_color']) : '#6c757d'
+        );
+    }
+
+    public function cm_heading_open_color_callback() {
+        printf(
+            '<input class="cm-color-picker" type="text" name="carrier_matrix_options[%s]" id="%s" value="%s">',
+            'cm_heading_open_color', 'cm_heading_open_color', isset( $this->carrier_matrix_options['cm_heading_open_color'] ) ? esc_attr( $this->carrier_matrix_options['cm_heading_open_color']) : '#495057'
+        );
+    }
+
+    public function cm_heading_hover_color_callback() {
+        printf(
+            '<input class="cm-color-picker" type="text" name="carrier_matrix_options[%s]" id="%s" value="%s">',
+            'cm_heading_hover_color', 'cm_heading_hover_color', isset( $this->carrier_matrix_options['cm_heading_hover_color'] ) ? esc_attr( $this->carrier_matrix_options['cm_heading_hover_color']) : '#343a40'
+        );
+    }
+
+    public function cm_heading_text_color_callback() {
+        printf(
+            '<input class="cm-color-picker" type="text" name="carrier_matrix_options[%s]" id="%s" value="%s">',
+            'cm_heading_text_color', 'cm_heading_text_color', isset( $this->carrier_matrix_options['cm_heading_text_color'] ) ? esc_attr( $this->carrier_matrix_options['cm_heading_text_color']) : '#ffffff'
+        );
+    }
+
+    public function carrier_matrix_shortcode_handler($atts) {
+        $carrier_matrix_options = get_option('carrier_matrix_options');
         $a = shortcode_atts( array(
             'form' => null, // product, abr, contact, or report
             'results' => null, // product, abr, contact, or report
-            'button_text' => 'Search',
-            'results_height' => null,
+            'button_text' => $carrier_matrix_options['cm_button_text'],
+            'results_height' => $carrier_matrix_options['cm_results_height'],
+            'cm_heading_color' => $carrier_matrix_options['cm_heading_color'],
+            'cm_heading_hover_color' => $carrier_matrix_options['cm_heading_hover_color'],
+            'cm_heading_open_color' => $carrier_matrix_options['cm_heading_open_color'],
+            'cm_heading_text_color' => $carrier_matrix_options['cm_heading_text_color'],
         ), $atts );
 
-        return $this->carrier_matrix_handler_callback($a['form'], $a['results'], $a['button_text'], $a['resultsHeight']);
+        return $this->carrier_matrix_handler_callback($a['form'], $a['results'], $a['button_text'], $a['results_height'], $a['cm_heading_color'], $a['cm_heading_hover_color'], $a['cm_heading_open_color'], $a['cm_heading_text_color']);
 
     }
 
     public function carrier_matrix_block_handler($atts) {
-        return $this->carrier_matrix_handler_callback($atts['form'], $atts['results'], $atts['button_text']);
+        return $this->carrier_matrix_handler_callback($atts['form'], $atts['results'], $atts['button_text'], $atts['results_height'], $atts['cm_heading_color'], $atts['cm_heading_hover_color'], $atts['cm_heading_open_color'], $atts['cm_heading_text_color']);
     }
 
-    public function carrier_matrix_handler_callback($form, $results, $button_text, $results_height) {
+    public function carrier_matrix_handler_callback($form, $results, $button_text, $results_height, $cm_heading_color, $cm_heading_hover_color, $cm_heading_open_color, $cm_heading_text_color) {
         wp_register_script( 'carrier-matrix-app', 'https://idacmstaging.wpengine.com/carrier-matrix/app.js' , '', time(), true );
         wp_enqueue_script('carrier-matrix-app');
-        $carrier_matrix_options = get_option('carrier_matrix_option_name');
+        $carrier_matrix_options = get_option('carrier_matrix_options');
 
         ob_start();
         if (empty($form) && empty($results)) {
@@ -195,7 +312,7 @@ class CarrierMatrixEasyInstallation {
         }
 
         if (!empty($form)) {
-            echo '<carrier-matrix form="'.$form.'" button-text="'.$button_text.'" api-key="'.$carrier_matrix_options['cm_api_key'].'" agency-id="'.$carrier_matrix_options['cm_agency_id'].'"></carrier-matrix>';
+            echo '<carrier-matrix form="'.$form.'" button-text="'.$button_text.'" heading-color="'.$cm_heading_color.'" heading-open-color="'.$cm_heading_open_color.'" heading-hover-color="'.$cm_heading_hover_color.'" heading-text-color="'.$cm_heading_text_color.'" api-key="'.$carrier_matrix_options['cm_api_key'].'" agency-id="'.$carrier_matrix_options['cm_agency_id'].'"></carrier-matrix>';
         } else if (!empty($results)) {
             echo '<carrier-matrix  results="'.$results.'" results-height="'.$results_height.'"></carrier-matrix>';
         }
---
+++ js/cm-color-picker.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+    let colorOptions = {
+        palettes: ['#444444','#ff2255','#559999','#99CCFF','#00c1e8','#F9DE0E','#111111','#EEEEDD']
+    }
+    $('.cm-color-picker').wpColorPicker(colorOptions)
+});
